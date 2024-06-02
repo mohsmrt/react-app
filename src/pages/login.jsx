a99882aeb32447d6d701e3e4fe4dfc5960a55ec3
Muhammad Asmarhamsyah <hamsyahsaud@gmail.com>
2024-06-02 19:46:05 +0800

    React router

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,15 @@
 import AuthLayout from "../components/layouts/AuthLayout";
 import FormLogin from "../components/fragments/FormLogin";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
     return (
         <AuthLayout title="Login">
             <FormLogin />
+            <p className="text-sm mt-5 text-center">
+                Don't have an account? <Link to="/register" className="font-bold text-blue-600">
+                    Register</Link>
+            </p>
         </AuthLayout>
     );
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,15 @@
 import AuthLayout from "../components/layouts/AuthLayout";
 import FormRegister from "../components/fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
     return (
         <AuthLayout title="Register">
             <FormRegister />
+            <p className="text-sm mt-5 text-center">
+                Already have an account? <Link to="/login" className="font-bold text-blue-600">
+                    Login</Link>
+            </p>
         </AuthLayout>
     );
 }
